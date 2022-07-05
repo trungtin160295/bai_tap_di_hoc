b2de67c74b90d70987dbd9e1950390796f9b16d8
pham_tin <trungtin160295@gmail.com>
2022-07-05 17:32:16 +0700

    dùng js để tạo danh sách bạn bè

--- facebook-/script.js
+++ facebook-/script.js
@@ -32,13 +32,47 @@ listAvatar.appendChild(avatar1);
 
 // tạo danh sách bạn bè
 
-var containerFriends =document.getElementById("container-friends");
+var containerFriends = document.getElementById("container-friend");
+  containerFriends.classList =("container-friend");
+  
+var containerFriendsHeader = document.createElement("div");
+  containerFriendsHeader.classList=("container-friends-header");
+  containerFriends.appendChild(containerFriendsHeader);
+var friend =document.createElement("a");
+  friend.href = "#";
+  friend.innerHTML="Bạn bè";
+  containerFriendsHeader.appendChild(friend);
+var seeAll =document.createElement("button");
+  seeAll.innerHTML = "Xem tất cả bạn bè";
+  containerFriendsHeader.appendChild(seeAll);
+var amountFriends = document.createElement("span");
+  amountFriends.innerHTML = "800 bạn bè";
+  containerFriends.appendChild(amountFriends);
+  
+var listImage = document.createElement("div");
+var group = document.createElement("div");
+group.classList = ["group-friends"];
 
-var containerFriendsHeader =document.createElement("div");
-containerFriends.appendChild(containerFriendsHeader);
-containerFriendsHeader.classList =["container-friends-header"];
-var friends = document.createElement("a");
-  friends.classList = "friends";
-  friends.innerHTML =("Bạn bè");
+for (let a = 0; a < 9; a++) {
+  let friend = document.createElement("div");
+  friend.classList = ["friend1"];
+  let avatar = document.createElement("img");
+  avatar.classList = ["avatar2"]
+    avatar.src="https://picsum.photos/200/300?random=" + a;
+   friend.appendChild(avatar);
+
+  let name = document.createElement("span");
+  name.innerHTML="bạn số " + a;
+  friend.appendChild(name)
+  group.appendChild(friend);
+}
+  
+
+listImage.appendChild(group);
+containerFriends.appendChild(listImage);
+
+
+
+
 
 
--- thu-js/script.js
+++ thu-js/script.js
@@ -1,14 +1,41 @@
 
-var listImage = document.getElementById("list-image");
-var group = document.createElement("div");
-group.classList = ["group-image"];
 
-for (let i = 0; i < 10; i++) {
-  let image = document.createElement("img");
-  image.src = "https://picsum.photos/200/300?random=" + i;
-  image.classList = ["image"];
- 
-  group.appendChild(image);
+
+var containerFriends = document.getElementById("container-friend");
+  containerFriends.classList =("container-friend");
+  
+var containerFriendsHeader = document.createElement("div");
+  containerFriendsHeader.classList=("container-friends-header");
+  containerFriends.appendChild(containerFriendsHeader);
+var friend =document.createElement("a");
+  friend.href = "#";
+  friend.innerHTML="Bạn bè";
+  containerFriendsHeader.appendChild(friend);
+var seeAll =document.createElement("button");
+  seeAll.innerHTML = "Xem tất cả bạn bè";
+  containerFriendsHeader.appendChild(seeAll);
+var amountFriends = document.createElement("span");
+  amountFriends.innerHTML = "800 bạn bè";
+  containerFriends.appendChild(amountFriends);
+  
+var listImage = document.createElement("div");
+var group = document.createElement("div");
+group.classList = ["group-friends"];
+
+for (let a = 0; a < 9; a++) {
+  let friend = document.createElement("div");
+  friend.classList = ["friend1"];
+  let avatar = document.createElement("img");
+  avatar.classList = ["avatar2"]
+    avatar.src="https://picsum.photos/200/300?random=" + a;
+   friend.appendChild(avatar);
+
+  let name = document.createElement("span");
+  name.innerHTML="bạn số " + a;
+  friend.appendChild(name)
+  group.appendChild(friend);
 }
+  
 
 listImage.appendChild(group);
+containerFriends.appendChild(listImage);
